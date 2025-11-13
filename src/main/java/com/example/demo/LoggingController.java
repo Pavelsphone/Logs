8b14b07bd7e1e599bc1ba27229c13282c2348eae
Pavelsphone <64527633+Pavelsphone@users.noreply.github.com>
2025-11-13 15:50:24 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/LoggingController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @GetMapping("/test")
+    public String testLogging() {
+        logger.debug("Это DEBUG сообщение");
+        logger.info("Это INFO сообщение");
+        return "Логи записаны!";
+    }
+}
+
